72b6b8a27ae86e5c1d98305c9ad7d6d49bbadd20
gxres042 <restent@tutanota.com>
2024-01-29 22:24:46 +0800

    fix: script sync from Misaka13514/Friends

--- index.ts
+++ index.ts
@@ -5,21 +5,31 @@ const {
   readFileSync,
   copySync,
   existsSync,
-  rmSync
+  rmSync,
 } = require("fs-extra");
+const path = require("path");
 const yaml = require("js-yaml");
 
-try {
- if (existsSync("./dist/")) {
-   rmSync("./dist/", { recursive: true, force: true });
- }
-  const listData = yaml.load(readFileSync("./src/list.yaml", "utf8"));
-  mkdirSync("./dist/");
-  copySync("./src/img", "./dist/img");
+const srcDir = path.join(__dirname, "src");
+const distDir = path.join(__dirname, "dist");
+const imgDir = path.join(srcDir, "img");
+const linkListPath = path.join(srcDir, "list.yaml");
+const imgPath = path.join(distDir, "img");
+const distPath = path.join(distDir, "linklist.json");
 
-  writeFileSync("./dist/links.json", JSON.stringify(listData));
+try {
+  const listData = yaml.load(readFileSync(linkListPath, "utf8"));
+
+  if (existsSync(distDir)) {
+    rmSync(distDir, { recursive: true, force: true });
+  }
+  mkdirSync(distDir);
+
+  copySync(imgDir, imgPath);
+  writeFileSync(distPath, JSON.stringify(listData, null, 2));
+
   console.log(listData);
-  console.log('Generate complete.');
+  console.log("List generate complete.");
 } catch (e) {
-  console.error(e);
+  console.error(e)
 }
